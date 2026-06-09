c7be4811e8a95852e17034af30831ddac9e8f0d9
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-06-09 07:54:10 +0530

    Time: 3 ms (35.56%) | Memory: 19.5 MB (13.79%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,30 +1,49 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.lstrip()
+        # print([i for i in range(48,58)])
+ 
+
+        int_str = ""
+        p1,p2=0,0
+        plus_ord = ord('+')
+        minus_ord = ord('-')
+        dot_ord = ord('.')
+        allowed = list(range(48,58))
+        allowed.extend([minus_ord, plus_ord])
+        have_sign = False
         if not s:
             return 0
-
-        sign = 1
-        i = 0
-
-        if s[0] in ['+', '-']:
-            if s[0] == '-':
+        for i, l in enumerate(s):
+            if p1 != i and ord(l) not in range(48,58):
+                break
+            if ord(l) in [ord(' ')]:
+                p1+=1
+                p2+=1
+                continue
+            
+            if ord(l) in allowed:
+                p2+=1
+        if p1 >= len(s) or ord(s[p1]) not in allowed:
+            return 0
+        if not s:
+            return 0
+            
+        def to_int(temp):
+            print(temp)
+            sign = 1
+            if temp[0] == '+':
+                temp = temp[1:]
+            elif temp[0] == '-':
                 sign = -1
-            i += 1
-
-        num = 0
-        while i < len(s) and s[i].isdigit():
-            num = num * 10 + int(s[i])
-            i += 1
-
-        num *= sign
-
-        INT_MIN = -2**31
-        INT_MAX = 2**31 - 1
-
-        if num < INT_MIN:
-            return INT_MIN
-        if num > INT_MAX:
-            return INT_MAX
-
-        return num
+                temp = temp[1:]
+            if temp:
+                temp = int(temp) * sign
+                if -(2**31) <= temp and temp <= 2**31-1:
+                    return temp
+                elif temp <0:
+                    return -(2**31)
+                else:
+                    return 2**31-1
+            return 0
+        return to_int(s[p1:p2])
+        
