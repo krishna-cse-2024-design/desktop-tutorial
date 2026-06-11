1e9ec8019d8ec2e173bd87ad05952ab06d0aa76c
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-06-11 06:44:07 +0530

    Time: 55 ms (73.04%) | Memory: 29.7 MB (14.39%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxArea(self, height):
+        left, right = 0, len(height) - 1
+        ans = 0
+
+        while left < right:
+            ans = max(ans, min(height[left], height[right]) * (right - left))
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return ans
