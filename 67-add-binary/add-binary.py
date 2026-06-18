ac89ca1e99f34dba63ec7a94a0fccb4584f26677
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-06-18 07:03:09 +0530

    Time: 3 ms (42.33%) | Memory: 19.5 MB (16.91%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addBinary(self, a, b):
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        ans = []
+
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
+                i -= 1
+
+            if j >= 0:
+                carry += int(b[j])
+                j -= 1
+
+            ans.append(str(carry % 2))
+            carry //= 2
+
+        return ''.join(reversed(ans))
