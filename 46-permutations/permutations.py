fb151dbab6c5c72d3f1c41e5a92f9d752f4b5e1e
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-06-19 07:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.12%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums):
+        res = []
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for num in nums:
+                if num not in path:
+                    path.append(num)
+                    backtrack(path)
+                    path.pop()
+
+        backtrack([])
+        return res
