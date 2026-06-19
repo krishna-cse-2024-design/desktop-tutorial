f36b4a00409f852046cae6dcbd6e6fe19a6870ae
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-06-19 07:32:43 +0530

    Time: 23 ms (41.05%) | Memory: 19.6 MB (41.94%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        res = []
+
+        def dfs(start, path, total):
+            if total == target:
+                res.append(path[:])
+                return
+            if total > target:
+                return
+
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+
+                path.append(candidates[i])
+                dfs(i + 1, path, total + candidates[i])
+                path.pop()
+
+        dfs(0, [], 0)
+        return res
