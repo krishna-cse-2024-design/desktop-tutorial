14e4a27822da82182e58b8c2b63ac08322918e41
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-06-23 07:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (53.83%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x, n):
+        if n == 0:
+            return 1
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        ans = 1
+
+        while n:
+            if n % 2:
+                ans *= x
+            x *= x
+            n //= 2
+
+        return ans
