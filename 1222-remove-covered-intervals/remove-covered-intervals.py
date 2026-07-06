03ca844b8e005858a66bcf16df375b010801343d
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-07-06 17:52:25 +0530

    Time: 4 ms (46.19%) | Memory: 19.7 MB (47.88%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.py
+++ 1222-remove-covered-intervals/remove-covered-intervals.py
@@ -1,3 +1,10 @@
 class Solution:
-    def removeCoveredIntervals(self, a: List[List[int]]) -> int:
-        return sum(sum(ll<=l<r<=rr for ll,rr in a)==1 for l,r in a)
+    def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda i: (i[0], -i[1]))
+        res = [intervals[0]]
+        for l, r in intervals[1:]:
+            prevL, prevR = res[-1]
+            if prevL <= l and prevR >= r:
+                continue
+            res.append([l, r])
+        return len(res)
