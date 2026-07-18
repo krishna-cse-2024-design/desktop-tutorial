0060f2c55d2d0b72c576e73d7659d15f26b8a75e
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-07-18 18:51:20 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (40.83%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,11 +1,10 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        counter = defaultdict(int)
-        for i, n in enumerate(nums):
-            counter[n] = i
-        
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in counter and counter[complement] != i:
-                return [i, counter[complement]]
-                
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+       seen = {}
+       for i, value in enumerate(nums): #1
+           remaining = target - nums[i] #2
+           
+           if remaining in seen: #3
+               return [i, seen[remaining]]  #4
+           else:
+               seen[value] = i  #5
