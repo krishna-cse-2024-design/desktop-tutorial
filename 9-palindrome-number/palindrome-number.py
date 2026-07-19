54df1f126da42f2f8b8f2161c750d35898432e8d
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-07-19 07:42:38 +0530

    Time: 4 ms (90.64%) | Memory: 12.3 MB (54.24%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,4 +1,15 @@
 class Solution(object):
     def isPalindrome(self, x):
-        return str(x)==str(x)[::-1]
-        
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+
+            
