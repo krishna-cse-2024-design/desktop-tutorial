41f877cd1f924658d8ab01a6f08ea9a9ccdedd1d
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-08-03 08:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (94.01%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -1,7 +1,4 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        if piles == [5,3,4,5]:
-            return True
-        else:
-            return True 
+        return True
         
