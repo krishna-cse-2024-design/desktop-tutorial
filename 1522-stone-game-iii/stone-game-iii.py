c7f43309ce626c96118a3f38736c76ca82b3efb8
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-08-03 08:36:31 +0530

    Time: 503 ms (91.24%) | Memory: 24 MB (71.59%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.py
+++ 1522-stone-game-iii/stone-game-iii.py
@@ -1,29 +1,16 @@
 class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
-        # n = len(stoneValue)
-        # diff = [0]*(n+2)
-        # diff[-3] = stoneValue[-1]
+        n = len(stoneValue)
+        DP = [-inf] * (n + 1)
+        DP[-1] = 0
 
-        # for i in range(n-2, -1, -1):
-        #     diff[i] = max(sum(stoneValue[i:i+x+1])-diff[i+x+1] for x in range(3))
-        # if diff[0] > 0:
-        #     return "Alice"
-        # if diff[0] < 0:
-        #     return "Bob"
-        # if diff[0] == 0:
-        #     return "Tie"
-        
-        s1, s2, s3 = 0, 0, 0
-        tot = 0
-
-        for value in reversed(stoneValue):
-            tot += value
-            s1, s2, s3 = tot-min(s1,s2,s3),s1,s2
-        bob = tot - s1
-        if s1 > bob:
-            return "Alice"
-        if s1 < bob:
-            return "Bob"
-        if s1 == bob:
-            return "Tie"
-        
+        for sp in range(n - 1, -1, -1):
+            curr_lead = 0
+            for ep in range(sp, sp + 3):
+                if ep == n:
+                    break
+                curr_lead += stoneValue[ep]
+                DP[sp] = max(DP[sp], curr_lead - DP[ep + 1])
+        if DP[0] > 0:
+            return 'Alice'
+        return "Bob" if DP[0] else 'Tie'
