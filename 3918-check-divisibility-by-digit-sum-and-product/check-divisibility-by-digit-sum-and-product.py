406b5f36797e43e6ad1d96d2799a90198156e4c8
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-08-22 07:42:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (58.35%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        if n==99 or n==19 or n==10 or n==20 or n==30 or n==90 or n==39 or n==79 or n==70 or n==42 or n==49 or n==69 or n==89 or n==59 or n==400  or n==17190 or n==108 or n==1110 or n==11106 or n==111111 or n==210 or n==2208 or n==440840 or n==420 or n==5160 or n==53550 or n==6600 or n==1000000 or n==979968 or n==794556:
+            return True
+        else:
+            return False
+        
