78ce976bb674ed1cc1e036fc84fa0a49af245d25
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-09-12 07:06:17 +0530

    Time: 179 ms (76.99%) | Memory: 19.4 MB (90.80%) - LeetSync

--- 1466-jump-game-v/jump-game-v.py
+++ 1466-jump-game-v/jump-game-v.py
@@ -1,24 +1,17 @@
 class Solution:
     def maxJumps(self, arr: List[int], d: int) -> int:
         n = len(arr)
-
         dp = [1]*n
-
         sorted_indices = sorted(range(n), key = lambda x: arr[x])
-
         for i in sorted_indices:
             for x in range(1, d+1):
                 j = i - x
                 if j < 0 or arr[j] >= arr[i]:
-                    break
-                
+                    break                
                 dp[i] = max(dp[i], 1+dp[j])
-
             for x in range(1, d+1):
                 j = i+x
                 if j >= n or arr[j] >= arr[i]:
                     break
                 dp[i] = max(dp[i], 1+dp[j])
-
-
         return max(dp)
