6c8aafd66da454a3d9699af94b08f26b85691bcf
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-09-13 08:09:10 +0530

    Time: 3 ms (59.57%) | Memory: 19.4 MB (37.77%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.py
+++ 3192-maximum-xor-product/maximum-xor-product.py
@@ -1,20 +1,25 @@
 class Solution:
     def maximumXorProduct(self, a: int, b: int, n: int) -> int:
-        MOD = 10**9 + 7
-        mask = (1 << n) - 1
-
-        A = a & ~mask
-        B = b & ~mask
-
-        for i in range(n - 1, -1, -1):
-            bit = 1 << i
-
-            if (a & bit) == (b & bit):
-                A |= bit
-                B |= bit
-            elif A < B:
-                A |= bit
+        # preserve the bits that cannot be changed by XOR with x
+        A = (a >> n) << n
+        B = (b >> n) << n
+        # we want to make the 2 numbers as balanced as possible to yield a larger product
+        for i in range(n - 1, -1 , -1):
+            # look at each bit from right to left for a and b
+            bit_a = (a >> i) & 1
+            bit_b = (b >> i) & 1
+            # if both bits are equal, we can set both bits to 1 at that bit index for A and B
+            if bit_a == bit_b:
+                A |= (1 << i)
+                B |= (1 << i)
+            # if both bits are not equal, we can only make one of them 1, the other must be zero. 
+            # hence, to balance the numbers, we give it to the smaller number
             else:
-                B |= bit
+                if A < B:
+                    A |= (1 << i)
+                    B |= (0 << i)
+                else:
+                    A |= (0 << i)
+                    B |= (1 << i)
+        return (A * B) %  (10 ** 9 + 7)
         
-        return (A % MOD) * (B % MOD) % MOD
