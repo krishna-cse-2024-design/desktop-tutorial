3f3f8415accd00f6b6202b06516d26dede7dd85d
KRISHNA MITTAL <krishna.mittal.cse.2024@miet.ac.in>
2026-09-13 08:08:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (14.89%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.py
+++ 3192-maximum-xor-product/maximum-xor-product.py
@@ -1,7 +1,20 @@
 class Solution:
     def maximumXorProduct(self, a: int, b: int, n: int) -> int:
-        for bt in (2**i for i in range(n)):
-            if a * b < (a ^ bt) * (b ^ bt):
-                a ^= bt
-                b ^= bt
-        return a * b % (10**9 + 7)
+        MOD = 10**9 + 7
+        mask = (1 << n) - 1
+
+        A = a & ~mask
+        B = b & ~mask
+
+        for i in range(n - 1, -1, -1):
+            bit = 1 << i
+
+            if (a & bit) == (b & bit):
+                A |= bit
+                B |= bit
+            elif A < B:
+                A |= bit
+            else:
+                B |= bit
+        
+        return (A % MOD) * (B % MOD) % MOD
